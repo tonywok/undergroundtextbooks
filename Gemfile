257d1133ef5521336cf3f9b501f904e30e1b442c
Tony Schneider <tonywok@gmail.com>
2010-08-03 08:04:38 -0400

    first commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem "rails",    "3.0.0.rc"
+gem "mongoid",  ">= 2.0.0.beta.15"
+gem "bson_ext", "1.0.4"
+gem "devise",   ">= 1.1"
+gem "haml"
+
+group :development do
+  gem "ruby-debug"
+end
+
+group :test do
+  gem "rspec-rails", ">= 2.0.0.beta.17"
+  gem "factory_girl_rails"
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Undergroundtextbooks::Application.config.secret_token = 'ebc3b4a4fe3a3cb3f9d9e7e39753c749b007f98c19b5e80f1f557733a25860e78608101e0e0c7f8a33d2671f5251dc04e5361e456e03f85925142eeeeb0e69a7'
