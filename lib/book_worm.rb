65c558788ec7f82dd23999c4aa82a78b89f1dee2
Tony Schneider <tonywok@gmail.com>
2010-08-14 13:26:04 -0400

    add some documentation to isbndb wrapper

--- lib/book_worm.rb
+++ lib/book_worm.rb
@@ -8,6 +8,12 @@ module BookWorm
 
   QUERY_BASE = "/api/books.xml?access_key=2I9YKZ6J"
 
+  # possible index/value pairs:
+  #   isbn     - pass in a valid isbn or isbn13 value.
+  #   title    - keyword search on title, long title and latinized title.
+  #   combined - search across titles, authors, and publisher name
+  #   full     - search across itles, authors, publisher name, summary, 
+  #              notes, awards information, etc
   def self.search(index1, value1)
     books = get(QUERY_BASE + "&index1=#{index1}&value1=#{CGI::escape(value1)}")
     books = [books['ISBNdb']['BookList']['BookData']].flatten.collect do |book|
@@ -18,4 +24,6 @@ module BookWorm
                :isbn13    => book["isbn13"])
     end
   end
+
+
 end
